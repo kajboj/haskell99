cbe57e95b14a883d81bc56195509833330787614
Kajetan Bojko <kai@shutl.co.uk>
2014-09-19 20:31:09 +0100

    removed unnecessary pattern matches in the definition of stack operations

--- stack.hs
+++ stack.hs
@@ -15,35 +15,21 @@ maxSeqLength = 5
 
 apply :: Operator -> Stack -> Maybe Stack
 apply Drop (x:xs) = Just xs
-apply Drop x = Nothing
 apply Nip (x1:x2:xs) = Just (x1:xs)
-apply Nip x = Nothing
 apply Over (x1:x2:xs) = Just (x2:x1:x2:xs)
-apply Over x = Nothing
 apply Dup (x:xs) = Just (x:x:xs)
-apply Dup x = Nothing
 apply Tuck (x1:x2:xs) = Just (x1:x2:x1:xs)
-apply Tuck x = Nothing
 apply Swap (x1:x2:xs) = Just (x2:x1:xs)
-apply Swap x = Nothing
 apply Rot (x1:x2:x3:xs) = Just (x3:x1:x2:xs)
-apply Rot x = Nothing
 apply MRot (x1:x2:x3:xs) = Just (x2:x3:x1:xs)
-apply MRot x = Nothing
 apply Drop2 (x1:x2:xs) = Just xs
-apply Drop2 x = Nothing
 apply Nip2 (x1:x2:x3:x4:xs) = Just (x1:x2:xs)
-apply Nip2 x = Nothing
 apply Dup2 (x1:x2:xs) = Just (x1:x2:x1:x2:xs)
-apply Dup2 x = Nothing
 apply Over2 (x1:x2:x3:x4:xs) = Just (x3:x4:x1:x2:x3:x4:xs)
-apply Over2 x = Nothing
 apply Tuck2 (x1:x2:x3:x4:xs) = Just (x1:x2:x3:x4:x1:x2:xs)
-apply Tuck2 x = Nothing
 apply Swap2 (x1:x2:x3:x4:xs) = Just (x3:x4:x1:x2:xs)
-apply Swap2 x = Nothing
 apply Rot2 (x1:x2:x3:x4:x5:x6:xs) = Just (x5:x6:x1:x2:x3:x4:xs)
-apply Rot2 x = Nothing
+apply _ _ = Nothing
 
 seqs :: Int -> [Sequence]
 seqs 0 = [[]]
@@ -70,4 +56,4 @@ allAnswers allSeqs input output =
 
 main :: IO ()
 main = do
-  putStrLn . show $ allAnswers allSeqs "abc" "cba"
+  putStrLn . show $ allAnswers allSeqs "abc" "abcc"
